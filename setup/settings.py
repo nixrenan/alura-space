128229a10f8fc07bcbaea298f469eb83c98d60c1
nixrenan <renanantunespr@gmail.com>
2023-11-22 10:51:14 -0300

    ALURA SPACE COMMIT 2

--- setup/settings.py
+++ setup/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3=*707!cw8=!=d!eirq58l*7oj!ttd&#9+@s+8wswbb97f)a^!"
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
